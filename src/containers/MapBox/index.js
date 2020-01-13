c09981262b5989564e548d7a4fa7652af2311b71
Pedro Araújo <araujo.miguelp@gmail.com>
2020-01-13 18:03:55 +0000

    Add crude implementation of inline map markers

--- src/containers/MapBox/index.js
+++ src/containers/MapBox/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import MapGL, { Layer, Source } from 'react-map-gl';
+import React, { Component, Fragment } from 'react';
+import MapGL, { Marker, Layer, Source } from 'react-map-gl';
 
 import Polyline from '@mapbox/polyline';
 
@@ -7,6 +7,12 @@ import './style.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const arrayEquals = (a1, a2) => a1.length === a2.length && a1.every(a => a2.includes(a));
+
+const MARKER_ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+const MARKER_SIZE = 20;
+const COLOR_INACTIVE = '#000000';
 
 const dataLayer = {
   id: 'data',
@@ -27,7 +33,7 @@ const dataLayerInactive = {
   source: 'routesInactive',
   paint: {
     'line-width': 3,
-    'line-color': '#000000'
+    'line-color': COLOR_INACTIVE
   }
 };
 
@@ -42,12 +48,18 @@ const dataLayerBorder = {
   }
 };
 
+const markerIcon = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+
 export default class Mapbox extends Component {
   state = {
     activeVehicles: [],
     routes: null,
     routesInactive: null,
-    uniqueHosts: null,
+    hosts: [],
+    hostsInactive: [],
+    depot: null,
 
     // Map Config
     hoveredFeature: null,
@@ -57,7 +69,7 @@ export default class Mapbox extends Component {
         'raster-tiles': {
             type: 'raster',
             tiles: [
-              'https://tile.osm.org/{z}/{x}/{y}.png'
+              'http://tile.osm.org/{z}/{x}/{y}.png'
             ],
             tileSize: 256
         }
@@ -86,26 +98,35 @@ export default class Mapbox extends Component {
       vehicleColors,
       vehicles: vehicleData,
       vehiclesPerDay,
-      vehiclesPerWeek
+      vehiclesPerWeek,
+      hosts
     } = nextProps;
 
     if (cur && cur.length > 0 && vehicleData && vehicleData.length > 0 &&
       !arrayEquals(cur, prev) ) {
-        // const vehicles = vehicleData.filter((_, i) => cur.includes(i));
-        const vehicles = [];
-        const inactiveVehicles = [];
+        const vehiclesActive = [];
+        const vehiclesInactive = [];
+        const hostsActive = [];
+        const hostsInactive = [];
+
+        const [longitude, latitude] = vehicleData[0].steps[0].location;
+        const depot = { longitude, latitude };
 
         vehicleData.forEach((v, i) => {
+          const { steps, vehicle } = v;
+
           if (cur.includes(i)) {
-            vehicles.push(v);
+            vehiclesActive.push(v);
+            steps.forEach(({ job }) => job !== undefined && hostsActive.push({ job, vehicle }));
           } else {
-            inactiveVehicles.push(v);
+            vehiclesInactive.push(v);
+            steps.forEach(({ job }) => job !== undefined && hostsInactive.push({ job, vehicle }));
           }
-        })
+        });
 
         const routes = {
           type: 'FeatureCollection',
-          features: vehicles.map(v => ({
+          features: vehiclesActive.map(v => ({
             type: 'Feature',
             geometry: Polyline.toGeoJSON(v.geometry),
             properties: {
@@ -119,7 +140,7 @@ export default class Mapbox extends Component {
 
         const routesInactive = {
           type: 'FeatureCollection',
-          features: inactiveVehicles.map(v => ({
+          features: vehiclesInactive.map(v => ({
             type: 'Feature',
             geometry: Polyline.toGeoJSON(v.geometry),
             properties: {
@@ -130,10 +151,33 @@ export default class Mapbox extends Component {
           }))
         };
 
+        const resolvedActiveHosts = hostsActive.reduce((acc, h) => {
+          const { job, vehicle } = h;
+          const resolved = hosts[job];
+
+          if (acc.find(({ host }) => host === resolved.host) === undefined) {
+            acc.push({ ...resolved, color: vehicleColors[vehicle][activeFilter] });
+          }
+          return acc;
+        }, []);
+
+        const resolvedInactiveHosts = hostsActive.reduce((acc, h) => {
+          const { job } = h;
+          const resolved = hosts[job];
+
+          if (acc.find(({ host }) => host === resolved.host) === undefined) {
+            acc.push({ ...resolved, color: COLOR_INACTIVE });
+          }
+          return acc;
+        }, []);
+
         return {
           activeVehicles: cur,
+          depot,
           routes,
-          routesInactive
+          routesInactive,
+          hosts: resolvedActiveHosts,
+          hostsInactive: resolvedInactiveHosts
         };
       }
 
@@ -177,6 +221,29 @@ export default class Mapbox extends Component {
   _renderTooltip() {
     const {hoveredFeature, x, y} = this.state;
 
+    if (!hoveredFeature) {
+      return null;
+    }
+
+    if (hoveredFeature.type === 'host') {
+      debugger;
+      const { host, perWeek, perMonth, nContainers, pickup } = hoveredFeature.host;
+      return (
+        <div className="tooltip" style={{left: x, top: y}}>
+          <div>Host: {host}</div>
+        </div>
+      );
+    } else {
+      debugger;
+      return (
+        <div className="tooltip" style={{left: x, top: y}}>
+          <div>Vehicle: {hoveredFeature.properties.vehicle + 1}</div>
+          <div>Day: {hoveredFeature.properties.day + 1}</div>
+          <div>Week: {hoveredFeature.properties.week + 1}</div>
+        </div>
+      );
+    }
+
     return (
       hoveredFeature && (
         <div className="tooltip" style={{left: x, top: y}}>
@@ -193,10 +260,17 @@ export default class Mapbox extends Component {
       mapStyle,
       routes,
       routesInactive,
-      viewport
+      viewport,
+
+      hosts,
+      hostsInactive,
+      depot
     } = this.state;
 
-    const { hosts, showAllRoutes } = this.props
+    const {
+      setActiveFeature,
+      showAllRoutes
+    } = this.props
 
     return (
       <div id="map">
@@ -221,6 +295,74 @@ export default class Mapbox extends Component {
                 </Source>
                 : null
             }
+            {
+              depot ?
+                <Marker latitude={depot.latitude} longitude={depot.longitude}>
+                  <svg
+                    height={MARKER_SIZE}
+                    viewBox={`0 0 ${MARKER_SIZE + 4} ${MARKER_SIZE + 4}`}
+                    style={{
+                      cursor: 'pointer',
+                      fill: 'red',
+                      stroke: 'none',
+                      transform: `translate(${-MARKER_SIZE / 2}px,${-MARKER_SIZE}px)`
+                    }}>
+                    <path
+                      stroke='black'
+                      strokeLinejoin='round'
+                      strokeWidth={1}
+                      d={MARKER_ICON} />
+                    <text x='50%' y='50%'
+                      fontWeight='bold'
+                      fontSize={10}
+                      fill='white'>
+                      D
+                    </text>
+                  </svg>
+                </Marker>
+                : null
+            }
+            <Fragment>
+            {
+              hosts.map((h, i) => {
+                const { color, host, latitude, longitude } = h;
+
+                return (
+                  <Marker
+                    key={host}
+                    longitude={longitude}
+                    latitude={latitude}>
+                    <svg
+                      onClick={() => setActiveFeature({ type: 'host', id: host })}
+                      onMouseEnter={ev => this._onHover({
+                        srcEvent: { offsetX: ev.screenX, offsetY: ev.screenY },
+                        features: [{ type: 'host', host: h, layer: { id: 'data' } }]
+                      })}
+                      height={MARKER_SIZE}
+                      viewBox={`0 0 ${MARKER_SIZE + 4} ${MARKER_SIZE + 4}`}
+                      style={{
+                        cursor: 'pointer',
+                        fill: color,
+                        stroke: 'none',
+                        transform: `translate(${-MARKER_SIZE / 2}px,${-MARKER_SIZE}px)`
+                      }}>
+                      <path
+                        stroke='black'
+                        strokeLinejoin='round'
+                        strokeWidth={1}
+                        d={MARKER_ICON} />
+                      <text x='50%' y='50%'
+                        fontWeight='bold'
+                        fontSize={10}
+                        fill='black'>
+                        {i}
+                      </text>
+                    </svg>
+                  </Marker>
+                );
+              })
+            }
+            </Fragment>
             {this._renderTooltip()}
         </MapGL>
       </div>
