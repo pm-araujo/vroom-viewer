c5a5a844ec55dced56ef8192b068668b3df81e13
Pedro Araújo <araujo.miguelp@gmail.com>
2020-01-15 15:36:06 +0000

    Updates

---
+++ precache-manifest.e00d872e78f3fc2eac9d5631f29651cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe48a2360a71f370288d5bf87548a7d",
+    "url": "/vroom-viewer/index.html"
+  },
+  {
+    "revision": "b9b2d701518be22f57b6",
+    "url": "/vroom-viewer/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "2f99578122a3ea0e7bed",
+    "url": "/vroom-viewer/static/css/main.520d8104.chunk.css"
+  },
+  {
+    "revision": "b9b2d701518be22f57b6",
+    "url": "/vroom-viewer/static/js/2.39b3aea9.chunk.js"
+  },
+  {
+    "revision": "196209d208e99f499b859cf00a09a7d4",
+    "url": "/vroom-viewer/static/js/2.39b3aea9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f99578122a3ea0e7bed",
+    "url": "/vroom-viewer/static/js/main.21c14dbe.chunk.js"
+  },
+  {
+    "revision": "133b7e247f459c9b9fd4",
+    "url": "/vroom-viewer/static/js/runtime-main.0f96f58b.js"
+  }
+]);
